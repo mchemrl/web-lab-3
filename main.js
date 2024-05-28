f76b8cfcde2a21463292dc9db7b6cf28f0d55051
mchemrl <mcchemro@gmail.com>
2024-05-28 21:12:21 +0300

    set up for js

---
+++ main.js
@@ -0,0 +1,22 @@
+function remember(event){
+
+}
+function rename(event){
+
+}
+function deleteProduct(event){
+
+}
+function buy(event){
+
+}
+function increase(event){
+
+}
+function decrease(event){
+    
+}
+function addProduct(event){
+
+}
+
